2aaa4f9751857afe9dea6b61542bf1c52bf0156e
Dominik <hi@domthedev.com>
2022-01-02 14:45:04 +0100

    Add files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+import {useEffect, useState} from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [characters, setCharacters] = useState([])
+    const [query, setQuery] = useState("")
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const {data} = await axios.get(`https://rickandmortyapi.com/api/character/?name=${query}`)
+                setCharacters(data.results)
+            } catch (error) {
+                console.error(error)
+            }
+        }
+
+        fetchData()
+    }, [query])
+
+    return (
+        <div className="App">
+            <div className="search">
+                <input type="text"
+                       placeholder={"Search Character"}
+                       className={"input"}
+                       onChange={event => setQuery(event.target.value)}
+                       value={query}
+                />
+            </div>
+            <div className="results">
+                {characters.map(character => (
+                    <div>
+                        <img src={character.image} alt={character.name}/>
+                        {character.name}
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 }
 
 export default App;
